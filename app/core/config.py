059b6de609f6a7a2b4dd28554be4c2522dd88485
github-jademon <j2python@gmail.com>
2025-05-10 20:06:18 +0900

    Feat: Add two new methods to newslist handling

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     NAVER_CLIENT_ID: str
     NAVER_CLIENT_SECRET: str
+    NEWS_API_KEY: str
+    THE_NEWS_API_KEY: str
 
     class Config:
         env_file = ".env"
--- crawlers/newslist.py
+++ crawlers/newslist.py
@@ -25,9 +25,56 @@ if(rescode==200):
     for item in data.get("items", []):
         get_news(item.get("originallink") or item.get("link"))
         print()
-
-    # print()
-    # print("전체 리스트")
-    # print(response_body.decode('utf-8'))
 else:
     print("Error Code:" + rescode)
+
+# --------------------------------------------------------------------------
+
+# https://newsapi.org/docs/endpoints/top-headlines
+# https://newsapi.org/account
+
+# from newsapi import NewsApiClient 
+
+# news_api_key = settings.NEWS_API_KEY
+
+# newsapi = NewsApiClient(api_key=news_api_key)
+
+# top_headlines = newsapi.get_top_headlines(country='us')
+
+# print(top_headlines)
+
+# --------------------------------------------------------------------------
+
+# https://www.thenewsapi.com/documentation#
+# https://www.thenewsapi.com/account/dashboard
+
+# the_news_api_key = settings.THE_NEWS_API_KEY
+
+# search = urllib.parse.quote("환경")
+# language = "ko"
+# limit = 3
+
+# url = f"https://api.thenewsapi.com/v1/news/sources?api_token={the_news_api_key}&language={language}"
+# url = f"https://api.thenewsapi.com/v1/news/all?api_token={the_news_api_key}&language={language}&limit={limit}&search={search}"
+
+# request = urllib.request.Request(url)
+# response = urllib.request.urlopen(request)
+# rescode = response.getcode()
+
+# if(rescode==200):
+#     response_body = response.read()
+#     data = json.loads(response_body.decode('utf-8'))
+    
+#     for item in data.get('data', []):
+#         print(item)
+
+#     for news_item in data.get('data', []):
+#         print(f"Title: {news_item.get('title')}")
+#         print(f"Description: {news_item.get('description')}")
+#         print(f"URL: {news_item.get('url')}")
+#         print(f"Published At: {news_item.get('published_at')}")
+#         print(f"Source: {news_item.get('source')}")
+#         print(f"Image URL: {news_item.get('image_url')}")
+#         print("-" * 50)
+# else:
+#     print("Error Code:" + rescode)
